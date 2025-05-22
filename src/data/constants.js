0bf64d59638c732146b46bc6faf291193414b628
Jayasree <jayasreeselvam37@gmail.com>
2025-05-22 10:07:00 +0530

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,10 +1,7 @@
 export const Bio = {
   name: "Jayasree S",
   roles: [
-    "Data Analyst",
-    "Machine learning",
-    "UI/UX Designer",
-    "Programmer",
+    "Quick learner",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
